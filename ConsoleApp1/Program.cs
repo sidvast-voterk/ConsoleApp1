63aed8b53345743af0b3b866981db24b1a63e4f6
Пользователь <@SIDVAST_VOTERK>
2020-01-16 18:06:32 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            //Console.WriteLine("Введите Ваше Имя");
+            //string fName = Console.ReadLine();
+            //Console.WriteLine("Введите Вашу Фамилию");
+            //string lName = Console.ReadLine();
+            //Console.WriteLine("Введите Ваш Возраст");
+            //byte age = Convert.ToByte(Console.ReadLine());
+            //Console.WriteLine("Введите Ваш Рост");
+            //byte rise = Convert.ToByte(Console.ReadLine());
+            //Console.WriteLine("Введите Ваш Вес");
+            //byte  heft = Convert.ToByte(Console.ReadLine());
+
+            //Console.WriteLine($"Имя:{fName} Фамилия:{lName} Возраст:{age} Рост:{rise} Вес:{heft}");
+
+            Console.ReadKey();
+        }
+    }
+}
